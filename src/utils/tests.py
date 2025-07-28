826c738d0a067d744682542fe6dceaa1a0ee553c
Texvss <149432383+Texvss@users.noreply.github.com>
2025-07-28 21:54:56 +0300

    Lil changes

--- src/utils/tests.py
+++ src/utils/tests.py
@@ -5,7 +5,7 @@ from torch.utils.data import DataLoader
 from src.datasets.asvspoof import ASVSpoofDataset
 from src.datasets.collate import collate_fn
 from src.model.lcnn import LCNNModel4
-
+from src.trainer.trainer import Trainer
 
 train_ds = ASVSpoofDataset("data/processed/train", "data/raw/ASVspoof2019_LA_cm_protocols/ASVspoof2019.LA.cm.train.trn.txt", "train")
 eval_ds = ASVSpoofDataset("data/processed/eval", "data/raw/ASVspoof2019_LA_cm_protocols/ASVspoof2019.LA.cm.eval.trl.txt", "eval")
@@ -16,7 +16,7 @@ eval_loader = DataLoader(eval_ds, batch_size=16, shuffle=True, collate_fn=collat
 dev_loader = DataLoader(dev_ds, batch_size=16, shuffle=True, collate_fn=collate_fn)
 
 batch = next(iter(train_loader))
-# print(batch["features"].shape, batch["labels"].shape)
+# print(batch["features"].shape, batch["labels"].shape) 
 
 model = LCNNModel4(
     input_channels=1,
