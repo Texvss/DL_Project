e5bc826e0d76a225fe4beab34161cddeb4df2bad
Texvss <149432383+Texvss@users.noreply.github.com>
2025-07-30 16:04:43 +0300

    Changes in LCNN class

--- src/model/lcnn.py
+++ src/model/lcnn.py
@@ -1,3 +1,4 @@
+# src/model/lcnn.py
 import torch
 from torch import nn
 
@@ -44,7 +45,11 @@ class LCNN(nn.Module):
             MFM(32, 32, kernel_size=3, padding=1),
             nn.MaxPool2d(kernel_size=2, stride=2)
         )
-        self.fc1 = nn.Linear(32 * 53 * 37, 160)
+        # global pooling -> flatten
+        self.global_pool = nn.AdaptiveAvgPool2d((1,1))
+        self.flatten = nn.Flatten()
+        # fully connected layers
+        self.fc1 = nn.Linear(32, 160)
         self.bn_fc1 = nn.BatchNorm1d(160)
         self.fc_mfm = nn.Linear(160, 80)
         self.bn_fc2 = nn.BatchNorm1d(80)
@@ -55,14 +60,15 @@ class LCNN(nn.Module):
         B, C, F, T = x.shape
         if T != self.TARGET_T:
             if T < self.TARGET_T:
-                x = torch.nn.functional.pad(x, (0, self.TARGET_T - T))
+                x = nn.functional.pad(x, (0, self.TARGET_T - T))
             else:
                 x = x[:, :, :, :self.TARGET_T]
         x = self.layer1(x)
         x = self.layer2(x)
         x = self.layer3(x)
         x = self.layer4(x)
-        x = x.view(B, -1)
+        x = self.global_pool(x)
+        x = self.flatten(x)
         x = self.fc1(x)
         x = self.bn_fc1(x)
         a, b = x.chunk(2, dim=1)
