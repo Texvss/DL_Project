26815fee031576a66b845d1f0198791cdca5fa1d
Texvss <149432383+Texvss@users.noreply.github.com>
2025-08-03 13:44:39 +0300

    Changes

--- src/model/angle_linear.py
+++ src/model/angle_linear.py
@@ -10,14 +10,18 @@ class AngleLinear(nn.Module):
         self.m = m
         self.s = s
 
-    def forward(self, x, labels):
+    def forward(self, x, labels=None):
         x_norm = F.normalize(x, dim=1)
         w_norm = F.normalize(self.weight, dim=1)
         cos_t = x_norm @ w_norm.t()
-        cos_t = cos_t.clamp(-1+1e-7, 1-1e-7)
-        theta = torch.acos(cos_t)
-        phi   = torch.cos(self.m * theta)
-        one_hot = torch.zeros_like(cos_t)
-        one_hot.scatter_(1, labels.view(-1,1), 1.0)
-        logits = self.s * (one_hot * phi + (1.0-one_hot) * cos_t)
+        cos_t = cos_t.clamp(-1 + 1e-7, 1 - 1e-7)
+
+        if labels is not None:
+            theta = torch.acos(cos_t)
+            phi = torch.cos(self.m * theta)
+            one_hot = torch.zeros_like(cos_t)
+            one_hot.scatter_(1, labels.view(-1, 1), 1.0)
+            logits = self.s * (one_hot * phi + (1.0 - one_hot) * cos_t)
+        else:
+            logits = self.s * cos_t
         return logits
